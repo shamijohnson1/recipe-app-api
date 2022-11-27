d1d314ea4aaeffbcb6f8f3520bcbd0be6b3f4caf
Shami Johnson <shawn.nisar@earthwavetech.com>
2022-11-27 18:28:15 -0500

    added tests

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator Functions
+"""
+
+def add(x, y):
+    """ Add x and y together and returns result"""
+    return x+y
+
+def subtract(x, y):
+    """ Subtract x and y together and returns result"""
+    return x-y
