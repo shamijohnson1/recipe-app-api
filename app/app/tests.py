3d9964302986343cb04db1a72f3c0df5a2d5f1c6
Shami Johnson <shawn.nisar@earthwavetech.com>
2022-11-27 18:32:59 -0500

    FIX LINTING ISSUES

--- app/app/tests.py
+++ app/app/tests.py
@@ -12,9 +12,8 @@ class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
         """Test adding numbers together"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
         """Test subtract numbers together"""
